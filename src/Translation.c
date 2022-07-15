5dacdbe9848c7cb31e5bcc49a2a45f10ddf89ac1
syatoss <syatoss@gmail.com>
2022-07-15 11:53:35 +0300

    added tables support to asmDescriptor

--- src/Translation.c
+++ src/Translation.c
@@ -31,6 +31,45 @@ unsigned long int binToDec(char* binary) {
     return dec;
 }
 
+char* uIntToBinary(unsigned int uInt) {
+    char* binUInt = (char*)malloc(sizeof(char) * WORD_SIZE + 1);
+    unsigned long int bit = 1;
+    int binUIntCurrentBit = WORD_SIZE - 1;
+    binUInt[WORD_SIZE] = '\0';
+    while (binUIntCurrentBit >= 0) {
+        binUInt[binUIntCurrentBit] = bit & uInt ? '1' : '0';
+        bit <<= 1;
+        binUIntCurrentBit--;
+    }
+    return binUInt;
+}
+
+void completionTo2(char* binVal) {
+    int bit = WORD_SIZE - 1;
+    while (bit >= 0) {
+        if (binVal[bit] == '0') {
+            bit--;
+        } else {
+            bit--;
+            break;
+        }
+        while (bit >= 0) {
+            binVal[bit] = binVal[bit] == '0' ? '1' : '0';
+            bit--;
+        }
+    }
+}
+
+char* intToBinary(int num) {
+    char* binInt;
+    unsigned int absVal;
+    int negative = num < 0;
+    absVal = negative ? -num : num;
+    binInt = uIntToBinary(absVal);
+    if (negative) completionTo2(binInt);
+    return binInt;
+}
+
 char* binaryToBase32(char* binary) {
     char* base32String = NULL;
     char msb[6];
